98c6b20c7f5edf63bca8c3704c11c697361e6ca2
DewiKresnawati <dewidesember20@gmail.com>
2025-09-09 09:45:28 +0700

    mongodb

---
+++ database/db.go
@@ -0,0 +1,35 @@
+package database
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"time"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+var DB *mongo.Database
+
+func ConnectDB() {
+	// ganti uri ini sesuai dengan Atlas / lokal kamu
+	uri := "mongodb://localhost:27017"
+
+	client, err := mongo.NewClient(options.Client().ApplyURI(uri))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	err = client.Connect(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// pilih database EducLex
+	DB = client.Database("educlex")
+	fmt.Println("✅ MongoDB Connected!")
+}

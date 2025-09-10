ff430bb922341f3a8cbea18006204a8b290cf498
DewiKresnawati <dewidesember20@gmail.com>
2025-09-10 09:55:03 +0700

    client

--- config/oauth.go
+++ config/oauth.go
@@ -8,8 +8,8 @@ import (
 // Ganti dengan Client ID & Client Secret dari Google Cloud Console
 var GoogleOauthConfig = &oauth2.Config{
 	RedirectURL:  "http://localhost:8080/auth/google/callback",
-	ClientID:     "YOUR_GOOGLE_CLIENT_ID",
-	ClientSecret: "YOUR_GOOGLE_CLIENT_SECRET",
+	ClientID:     "778838656131-jfnap1huoa7igvob44b1159gg0e2q99e.apps.googleusercontent.com ",
+	ClientSecret: "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",
 	Scopes: []string{
 		"https://www.googleapis.com/auth/userinfo.email",
 		"https://www.googleapis.com/auth/userinfo.profile",
